eb9f17cb41ca50a6c50ecd949b332d9ec0e0ebfb
unknown <kimitawanjohi7923@gmail.com>
2021-07-22 21:36:25 +0300

    main db models done

--- ecom/settings.py
+++ ecom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
+    'ariadne.contrib.django'
 ]
 
 MIDDLEWARE = [
@@ -117,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join( BASE_DIR, 'media' )
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static files')
+STATICFILES_DIRS = [
+    os.path.join( BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
---
+++ main/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+from django.db.models import base
+
+from users.models import User
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    contact = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    price = models.FloatField()
+    discout = models.FloatField(default=0)
+    image = models.ImageField(upload_to= 'product images')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.product.name} order-item'
+
+
+class Order(models.Model):
+    order_item = models.ManyToManyField(OrderItem)
+    paid = models.BooleanField(default=False)
+    total = models.FloatField()
+
+    def __str__(self):
+        return self.order_item.product.name + 'order'
