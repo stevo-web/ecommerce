aa82b5f789429bc56d4f9240c65ba163723d3483
unknown <kimitawanjohi7923@gmail.com>
2021-08-20 00:21:12 +0300

    done with local auth

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, logout, login as _login
+from django.contrib.auth import authenticate, logout as _logout, login as _login
 from .models import User
 
 
@@ -10,5 +10,25 @@ def register(request):
 
 def login(request):
     context = {}
+    user = request.user
+    if user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
 
+        user = authenticate(
+            email=email,
+            password=password
+        )
+        if user is not None:
+            _login(request, user)
+            return redirect('home')
+        else:
+            context["err"] = 'invalid username or password'
     return render(request, 'login.html', context)
+
+
+def logout(request):
+    _logout(request)
+    return redirect('home')
