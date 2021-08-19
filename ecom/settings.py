400a374e46518026f9eb76c8ae3e3100153915a0
unknown <kimitawanjohi7923@gmail.com>
2021-08-19 05:27:12 +0300

    dns settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-+6$+&j#a8#op*hefmn5_&*x#4@$##b27yx)h&oe9!-0@fje7a@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['143.244.137.74', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['143.244.137.74', '127.0.0.1', 'localhost', 'www.elsoko.co', 'elsoko.co']
 
 
 # Application definition
