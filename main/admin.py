e4389d3abb599cb696ae14203d41634607bae5c7
unknown <kimitawanjohi7923@gmail.com>
2021-10-09 11:01:56 +0300

    admin site changes

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from . models import Shop, Category, SubCategory, Product, OrderItem, Order
 
+admin.site.site_header = "Elsoko Admin"
+admin.site.site_title = "Elsoko Admin Portal"
+admin.site.index_title = "Welcome to Elsoko Admin Portal"
+
 admin.site.register(Shop)
 admin.site.register(Category)
 admin.site.register(SubCategory)
